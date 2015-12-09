b8061653d93537c891da33d9dfd036ce582f8ec9
William Makabenta <wmakaben@gmail.com>
2015-12-09 14:12:50 -0500

    Added mark incorrect and ignore features

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -29,14 +29,31 @@ $(document).ready(function(){
 	});
 
 	$('#tweet-filter input:checkbox').change(function(){
-        $(this).is(':checked') ? $('.' + $(this).val()).show() : $('.' + $(this).val()).hide();
+		applyFilters();
     });
+
+	$(document).on('click', '.incorrect-button', function(){
+		markIncorrect($(this).closest('.tweet'));
+	});
+
+	$(document).on('click', '.correct-button', function(){
+		markCorrect($(this).closest('.tweet'));
+	});
+
+	$(document).on('click', '.remove-button', function(){
+		ignoreTweet($(this).closest('.tweet'));
+	});
+
+	$(document).on('click', '.add-button', function(){
+		includeTweet($(this).closest('.tweet'));
+	});
 });
 
 function lookupPlayer(name){
 	$('#more-results').hide();
 	$('#tweet-list').empty();
 	$('#tweet-filter input').prop('checked', true);
+	$('#rem-check').prop('checked', false);
 
 	if($.trim(name) == '')
 		showResult("Please Enter a Name");
@@ -90,28 +107,120 @@ function addTweets(score, tweets){
 }
 
 function addToTweetList(tweet, index){
-	$('#tweet-list').append($('<div class="tweet">').addClass(tweet.classification)
+	$('#tweet-list').append($('<div class="tweet correct included">').addClass(tweet.classification)
 		.append($('<div class="tweet-index">').text(index))
 		.append($('<div class="tweet-text">').text(tweet.text))
 		.append($('<div class="tweet-info">')
 			.append($('<div class="tweet-retweets">').text("Retweets: " + tweet.retweet_count))
 			.append($('<div class="tweet-favorites">').text("Favorites: " + tweet.favorite_count))
-			.append($('<div class="tweet-class">').text("Class: " + tweet.classification))
+			.append($('<div class="tweet-class">').text(tweet.classification))
+			.append($('<button class="incorrect-button">').text("Switch Class"))
+			.append($('<button class="remove-button">').text("Remove"))
 		)
 	);
 }
 
+function applyFilters(){
+	$('.tweet').show();
+	unchecked = $('#tweet-filter').find('input:checkbox:not(:checked)');
+    unchecked.each(function(i){
+       	$('.' + $(this).val()).hide();
+    });
+}
 
+function markIncorrect(tweet){
+	tweet.removeClass('correct').addClass('incorrect');
+	classDiv = tweet.find('.tweet-class');
+	curClass = classDiv.text();
+	newClass = (curClass=='pos' ? 'neg' : 'pos')
+	tweet.removeClass(curClass).addClass(newClass);
+	classDiv.text(newClass);
+	$('#num-incorrect span').text(parseInt($('#num-incorrect span').text()) + 1);
+	$('#' + newClass + '-num span').text(parseInt($('#' + newClass + '-num span').text()) + 1);
+	$('#' + curClass + '-num span').text(parseInt($('#' + curClass + '-num span').text()) - 1);
+	$('#num-incorrect-' + curClass + ' span').text(parseInt($('#num-incorrect-' + curClass + ' span').text()) + 1);
 
+	tweet.find($('.incorrect-button'))
+		.removeClass('incorrect-button')
+		.addClass('correct-button')
 
+	applyFilters();
+}
 
+function markCorrect(tweet){
+	tweet.removeClass('incorrect').addClass('correct');
+	classDiv = tweet.find('.tweet-class');
+	curClass = classDiv.text();
+	newClass = (curClass=='pos' ? 'neg' : 'pos')
+	classDiv.text(newClass);
+	tweet.removeClass(curClass).addClass(newClass);
 
+	$('#num-incorrect span').text(parseInt($('#num-incorrect span').text()) - 1);
+	$('#' + newClass + '-num span').text(parseInt($('#' + newClass + '-num span').text()) + 1);
+	$('#' + curClass + '-num span').text(parseInt($('#' + curClass + '-num span').text()) - 1);
+	$('#num-incorrect-' + newClass + ' span').text(parseInt($('#num-incorrect-' + newClass + ' span').text()) - 1);
 
+	tweet.find($('.correct-button'))
+		.removeClass('correct-button')
+		.addClass('incorrect-button');
 
+	applyFilters();
+}
 
+function ignoreTweet(tweet){
+	tweet.removeClass('included').addClass('removed');
+	classDiv = tweet.find('.tweet-class');
+	curClass = classDiv.text();
+	$('#num-removed span').text(parseInt($('#num-removed span').text()) + 1);
+	$('#' + curClass + '-num span').text(parseInt($('#' + curClass + '-num span').text()) - 1);
 
+	if($(this).hasClass('incorrect')){
+		$('#num-incorrect span').text(parseInt($('#num-incorrect span').text()) - 1);
+		originalClass = (curClass=='pos' ? 'neg' : 'pos');
+		$('#num-incorrect-' + originalClass + ' span').text(parseInt($('#num-incorrect-' + originalClass + ' span').text()) - 1);
+	}
 
+	tweet.find($('.remove-button'))
+		.removeClass('remove-button')
+		.addClass('add-button')
+		.text('Included');
 
+	tweet.find('.incorrect-button').prop('disabled',true);
+	tweet.find('.correct-button').prop('disabled',true);
+
+	applyFilters();
+}
+
+function includeTweet(tweet){
+	tweet.removeClass('removed').addClass('included');
+	curClass = tweet.find('.tweet-class').text();
+	$('#num-removed span').text(parseInt($('#num-removed span').text()) - 1);
+	$('#' + curClass + '-num span').text(parseInt($('#' + curClass + '-num span').text()) + 1);
+
+	tweet.find($('.add-button'))
+		.removeClass('add-button')
+		.addClass('remove-button')
+		.text('Remove');
+
+	if($(this).hasClass('incorrect')){
+		$('#num-incorrect span').text(parseInt($('#num-incorrect span').text()) + 1);
+		originalClass = (curClass=='pos' ? 'neg' : 'pos');
+		$('#num-incorrect-' + originalClass + ' span').text(parseInt($('#num-incorrect-' + originalClass + ' span').text()) + 1);
+	}
+
+	tweet.find('.incorrect-button').prop('disabled',false);
+	tweet.find('.correct-button').prop('disabled',false);
+
+	applyFilters();
+}
+
+function calcPercentages(){
+
+}
+
+function addToTraining(tweet){
+
+}
 
 
 
